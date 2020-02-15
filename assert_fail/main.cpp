eb1d2fcf403576b2b07bd4161a6c69e8f74a465b
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-02-14 16:35:27 -0800

    sycl::exception caught: The program was built for 1 devices

--- assert_fail/main.cpp
+++ assert_fail/main.cpp
@@ -1,12 +1,24 @@
-#include <AMReX.H>
-
-using namespace amrex;
+#include <CL/sycl.hpp>
+namespace sycl = cl::sycl;
 
 int main (int argc, char* argv[])
 {
-    amrex::Initialize(argc,argv);
     {
-
+        sycl::gpu_selector my_selector;
+        sycl::device my_device(my_selector);
+        sycl::context my_context(my_device);
+        sycl::ordered_queue q(my_context, my_selector);
+        try {
+            q.submit([&] (sycl::handler& h) {
+                h.parallel_for(sycl::nd_range<1>(sycl::range<1>(8),sycl::range<1>(8)),
+                [=] (sycl::nd_item<1> item)
+                {
+                    //
+                });
+                });
+        } catch (sycl::exception const& ex) {
+            std::cout << "sycl::exception caught: " << ex.what() << std::endl;
+        }
     }
-    amrex::Finalize();
+    std::cout << "End of program\n";
 }
