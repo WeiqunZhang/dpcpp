3d436b94a22ef5554875743b890e550698f94291
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-02-24 09:17:45 -0800

    update abs

--- abs/main.cpp
+++ abs/main.cpp
@@ -2,9 +2,14 @@
 namespace sycl = cl::sycl;
 
 #include <iostream>
+#include <type_traits>
 
 int main (int argc, char* argv[])
 {
     std::cout << " sycl::abs(-6) = " <<  sycl::abs(-6) << "\n"
               << "-sycl::abs(-6) = " << -sycl::abs(-6) << "\n";
+    auto tsycl = sycl::abs(-6);
+    auto tstd  =  std::abs(-6);
+    std::cout << "Is sycl::abs(-6) signed? " << std::is_signed<decltype(tsycl)>::value << "\n"
+              << "Is  std::abs(-6) signed? " << std::is_signed<decltype(tstd )>::value << "\n";
 }
